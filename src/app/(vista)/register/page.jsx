ed44845cb3910c2dcf397239b1838a5d79471844
igrisdev <johanalvarezz237@gmail.com>
2024-05-10 15:14:47 -0500

    view of register finished

--- src/app/(vista)/register/page.jsx
+++ src/app/(vista)/register/page.jsx
@@ -3,12 +3,39 @@ export default function Register() {
     <main className='grid place-content-center h-screen'>
       <div class='max-w-md relative flex flex-col p-4 rounded-md text-black bg-white'>
         <div class='text-2xl font-bold mb-2 text-[#1e0e4b] text-center'>
-          Welcome back to <span class='text-[#7747ff]'>App</span>
-        </div>
-        <div class='text-sm font-normal mb-4 text-center text-[#1e0e4b]'>
-          Log in to your account
+          Regístrate en <span class='text-[#7747ff]'>Horario UniMayor</span>
         </div>
         <form class='flex flex-col gap-3'>
+          <div className='flex items-center mt-2 gap-4'>
+            <div class='block relative '>
+              <label
+                for='firstName'
+                class='block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2'
+              >
+                Nombre
+              </label>
+              <input
+                type='text'
+                id='firstName'
+                name='firstName'
+                class='rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2  ring-gray-900 outline-0'
+              />
+            </div>
+            <div class='block relative'>
+              <label
+                for='lastName'
+                class='block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2'
+              >
+                Apellido/s
+              </label>
+              <input
+                type='text'
+                id='lastName'
+                name='lastName'
+                class='rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2  ring-gray-900 outline-0'
+              />
+            </div>
+          </div>
           <div class='block relative'>
             <label
               for='email'
@@ -19,6 +46,7 @@ export default function Register() {
             <input
               type='text'
               id='email'
+              name='email'
               class='rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2  ring-gray-900 outline-0'
             />
           </div>
@@ -27,36 +55,44 @@ export default function Register() {
               for='password'
               class='block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2'
             >
-              Password
+              Contraseña
             </label>
             <input
               type='text'
               id='password'
+              name='password'
               class='rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0'
             />
           </div>
-          <div>
-            <a
-              class='text-sm text-[#7747ff]'
-              href='#'
+
+          <div class='block relative'>
+            <label
+              for='revalidatePassword'
+              class='block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2'
             >
-              Forgot your password?
-            </a>
+              Misma contraseña
+            </label>
+            <input
+              type='text'
+              id='revalidatePassword'
+              class='rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0'
+            />
           </div>
+
           <button
             type='submit'
             class='bg-[#7747ff] w-max m-auto px-6 py-2 rounded text-white text-sm font-normal'
           >
-            Submit
+            Crear cuenta
           </button>
         </form>
-        <div class='text-sm text-center mt-[1.6rem]'>
-          Don’t have an account yet?{' '}
+        <div class='flex gap-1 items-center justify-center mt-3'>
+          Ya tiene cuenta
           <a
-            class='text-sm text-[#7747ff]'
-            href='#'
+            class='text-[#7747ff]'
+            href='/login'
           >
-            Sign up for free!
+            Inicia Sesión
           </a>
         </div>
       </div>
