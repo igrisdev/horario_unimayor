5a891160e79ee7090e7a383bb528a2e6cbf05298
igrisdev <johanalvarezz237@gmail.com>
2024-05-10 14:59:54 -0500

    modifier the bg of layout

--- src/app/(vista)/layout.jsx
+++ src/app/(vista)/layout.jsx
@@ -12,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className + ' h-screen'}>{children}</body>
+      <body className={inter.className + ' bg-[#16161d] text-gray-400'}>
+        {children}
+      </body>
     </html>
   )
 }
