6fbbe7e8dc4964518006fa20b38f9c01fb9585f2
igrisdev <johanalvarezz237@gmail.com>
2024-05-10 16:47:43 -0500

    POST, GET process

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -3,22 +3,33 @@ import connectDB from '@/lib/connectDB'
 
 import { NextResponse } from 'next/server'
 
+export async function GET() {
+  try {
+    await connectDB()
+
+    const users = await Users.find({}, { createdAt: 0, updatedAt: 0, __v: 0 })
+
+    return NextResponse.json(users, { status: 200 })
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    )
+  }
+}
+
 export async function POST(req) {
   try {
     await connectDB()
 
-    const { firstName, lastName, email, password } = req.body
+    const { firstName, lastName, email, password } = await req.json()
 
-    const person = new Users({
+    await Users.create({
       firstName,
       lastName,
       email,
       password,
     })
-
-    await person.save()
-
-    console.log('inside api', person)
 
     return NextResponse.json(
       { message: 'Usuario creado con éxito' },
