b28313e44d61623d96ffffcd887d437dafe95d59
igrisdev <johanalvarezz237@gmail.com>
2024-05-12 21:42:56 -0500

    test 1 of fixed bug

--- src/app/(vista)/dashboard/subject/page.jsx
+++ src/app/(vista)/dashboard/subject/page.jsx
@@ -5,12 +5,10 @@ import { TableSubject } from '@/components/dashboard/subject/TableSubject'
 export default async function Subject() {
   async function getSubjects() {
     const { data } = await Axios.get('/api/dashboard/subject')
-    console.log(data)
     return data
   }
 
   const dataSubjects = await getSubjects()
-  console.log(dataSubjects)
 
   return (
     <main>
