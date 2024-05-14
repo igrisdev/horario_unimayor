9e63a889630875cf94241926f3156f91fb4a0857
igrisdev <johanalvarezz237@gmail.com>
2024-05-14 01:55:22 -0500

    update create subject to server actions

--- src/lib/actions/subject/actionSubject.js
+++ src/lib/actions/subject/actionSubject.js
@@ -4,6 +4,14 @@ import { Axios } from '@/lib/axios'
 import prisma from '@/lib/prisma'
 import { revalidatePath } from 'next/cache'
 
+export async function createSubject(formData) {
+  const subject = Object.fromEntries(formData)
+
+  Axios.post('/api/dashboard/subject', subject)
+
+  revalidatePath('/dashboard/subject')
+}
+
 export async function deleteSubject(formData) {
   const id = formData.get('id')
 
