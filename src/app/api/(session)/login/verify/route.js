53be146af88ee7d4bcb3d3ad515802451264841d
igrisdev <johanalvarezz237@gmail.com>
2024-05-29 00:04:27 -0500

    test 26

--- src/app/api/(session)/login/verify/route.js
+++ src/app/api/(session)/login/verify/route.js
@@ -3,14 +3,9 @@ import { verifyJWT } from '@/lib/verifyJWT'
 import jwt from 'jsonwebtoken'
 
 export async function GET(req) {
-  const token = req.cookies.get('token')
+  const token = await req.cookies.get('token')
 
-  console.log(token)
-
-  const id = jwt.verify(
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsd2I2aDdhYjAwMDFtMzkyY3JnaHlwcDAiLCJpYXQiOjE3MTY5NTQ4ODEsImV4cCI6MTcxNzA0MTI4MX0.BzJpx734yPOBYNG5uEuP7ASzm-8c-bHW0cQ4i_pYoPI',
-    process.env.JWT_SECRET
-  )
+  const id = jwt.verify(token.value.toString(), process.env.JWT_SECRET)
 
   console.log(id)
   return NextResponse.json({ id })
