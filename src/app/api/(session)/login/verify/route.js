7e57e995468e910734b20be4732bba073e139c00
igrisdev <johanalvarezz237@gmail.com>
2024-05-28 23:57:38 -0500

    test 23

--- src/app/api/(session)/login/verify/route.js
+++ src/app/api/(session)/login/verify/route.js
@@ -5,7 +5,13 @@ import jwt from 'jsonwebtoken'
 export async function GET(req) {
   const token = req.cookies.get('token')
 
-  const { id } = jwt.verify(token.value, process.env.JWT_SECRET)
+  console.log(token)
 
+  const id = jwt.verify(
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsd2I2aDdhYjAwMDFtMzkyY3JnaHlwcDAiLCJpYXQiOjE3MTY5NTQ4ODEsImV4cCI6MTcxNzA0MTI4MX0.BzJpx734yPOBYNG5uEuP7ASzm-8c-bHW0cQ4i_pYoPI',
+    'palabrasupersecreta'
+  )
+
+  console.log(id)
   return NextResponse.json({ id })
 }
