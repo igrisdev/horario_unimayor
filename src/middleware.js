4324b4431cac8345a36d98f396b814a3e4325422
igrisdev <johanalvarezz237@gmail.com>
2024-05-29 21:42:27 -0500

    fixed bug when you go to the register, the page redirect you to the login

--- src/middleware.js
+++ src/middleware.js
@@ -12,11 +12,10 @@ export function middleware(req) {
     return NextResponse.redirect(new URL('/', req.url))
   }
 
-  // En cualquier otro caso, continuar con la solicitud
   return NextResponse.next()
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/dashboard/:path*', '/', '/login', '/register'],
+  matcher: ['/dashboard/:path*', '/', '/login'],
 }
